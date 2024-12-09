34d68bb347962c2d946cf243826e3acdebe54c6d
s1114707 <xuxiang0707@gmail.com>
2024-12-09 16:14:35 +0800

    V3-許皓翔

--- app/src/main/java/com/example/s1114707/MainActivity.kt
+++ app/src/main/java/com/example/s1114707/MainActivity.kt
@@ -5,13 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,10 +42,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
+    // 定義背景顏色的狀態
+    var currentColor by remember { mutableStateOf(Color(0xff95fe95)) }
+
+    // 顏色列表
+    val colors = listOf(
+        Color(0xff95fe95),
+        Color(0xfffdca0f),
+        Color(0xfffea4a4),
+        Color(0xffa5dfed)
+    )
+
+    // 設置閾值，只有當滑動的距離超過閾值時才會切換顏色
+    val dragThreshold = 200f // 可以根據需要調整
+
+    // 根據滑動方向切換顏色
     Box(
         modifier = Modifier
-            .background(Color(0xff95fe95)) // 設定背景顏色
+            .background(currentColor) // 使用當前顏色
             .fillMaxSize() // 使 Box 充滿螢幕
+            .pointerInput(Unit) {
+                detectHorizontalDragGestures { _, dragAmount ->
+                    // 只有當滑動距離超過閾值才觸發顏色切換
+                    if (dragAmount > dragThreshold) {
+                        currentColor = getNextColor(colors, currentColor)
+                    } else if (dragAmount < -dragThreshold) {
+                        currentColor = getPreviousColor(colors, currentColor)
+                    }
+                }
+            }
     ) {
         Column(
             modifier = Modifier
@@ -98,6 +125,26 @@ fun MyApp() {
                 Text(text = "結束App")
             }
         }
+    }
+}
+
+// 根據當前顏色獲取下一個顏色
+fun getNextColor(colors: List<Color>, currentColor: Color): Color {
+    val currentIndex = colors.indexOf(currentColor)
+    return if (currentIndex == colors.size - 1) {
+        colors[0] // 如果是最後一個顏色，則返回第一個顏色
+    } else {
+        colors[currentIndex + 1]
+    }
+}
+
+// 根據當前顏色獲取上一個顏色
+fun getPreviousColor(colors: List<Color>, currentColor: Color): Color {
+    val currentIndex = colors.indexOf(currentColor)
+    return if (currentIndex == 0) {
+        colors[colors.size - 1] // 如果是第一個顏色，則返回最後一個顏色
+    } else {
+        colors[currentIndex - 1]
     }
 }
 
